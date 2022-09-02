e21396ab1e9188664bd3f99b9396a42cab115a8b
Jeff <jefnic23@gmail.com>
2022-09-02 19:00:31 -0400

    db cleanup

--- hev.py
+++ hev.py
@@ -32,17 +32,17 @@ if __name__ == '__main__':
     warnings.filterwarnings('ignore')
 
     # initialize progress bar
-    pbar = tqdm(total=100, position=0, desc="Overall")
+    pbar = tqdm(total=4, position=0, desc="Overall")
 
     # make directory to house leaderboards
     if not os.path.exists('data'):
         os.mkdir('data')
 
     # get player id data and read into a pandas dataframe
-    url = "https://raw.githubusercontent.com/chadwickbureau/register/master/data/people.csv"
-    lookup = pd.read_csv(url, index_col="key_mlbam")
+    url            = "https://raw.githubusercontent.com/chadwickbureau/register/master/data/people.csv"
+    lookup         = pd.read_csv(url, index_col="key_mlbam")
     lookup['name'] = lookup['name_first'] + ' ' + lookup['name_last']
-    pbar.update(25)
+    pbar.update(1)
 
     # select necessary columns from database only
     columns = [
@@ -56,19 +56,14 @@ if __name__ == '__main__':
 
     # get savant data from postgres and read into pandas dataframe
     engine = createEngine()
-    df = pd.read_sql('baseball_savant', engine, columns=columns)
-    df['game_year'] = df['game_year'].astype(int)
-    df['batter'] = df['batter'].astype(int)
-    df['launch_speed'] = df['launch_speed'].astype(float)
-    df['launch_angle'] = df['launch_angle'].astype(float)
-    pbar.update(25)
+    df     = pd.read_sql('baseball_savant', engine, columns=columns)
+    pbar.update(1)
 
     # clean up data and calculate hEV
     df = df[['batter', 'game_year', 'launch_speed', 'launch_angle', 'woba_denom', 'bb_type']].fillna(0)
-    df['woba_denom'] = df['woba_denom'].astype(float)
     df['hEV'] = np.where(df['bb_type'] != 0, df['launch_speed'] * np.cos(np.radians(df['launch_angle'] - 25)), 0)
     df['BBE'] = np.where(df['bb_type'] != 0, 1, 0)
-    pbar.update(25)
+    pbar.update(1)
 
     # create .xlsx file
     with pd.ExcelWriter('data/hEV_leaderboards.xlsx') as writer:
@@ -79,8 +74,8 @@ if __name__ == '__main__':
             # groupby batters and get average hEV
             df_year = df_year.groupby('batter').agg({'hEV': 'sum', 'BBE': 'sum', 'woba_denom': 'sum'})
             df_year.rename(columns={'woba_denom': 'PA'}, inplace=True)
-            df_year['hEV/PA'] = df_year['hEV'] / df_year['PA']
+            df_year['hEV/PA']  = df_year['hEV'] / df_year['PA']
             df_year['hEV/BBE'] = df_year['hEV'] / df_year['BBE']
-            df_year.index = df_year.index.map(name)
+            df_year.index      = df_year.index.map(name)
             df_year[['BBE', 'PA', 'hEV/BBE', 'hEV/PA']].dropna().sort_values(by=['hEV/PA'], ascending=False).to_excel(writer, sheet_name=str(year))
-    pbar.update(25)
+    pbar.update(1)
